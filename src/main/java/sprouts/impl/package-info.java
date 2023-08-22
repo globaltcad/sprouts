36ddd9cdadb54e0c2c6e81decae5647b70c72a0d
Daniel Nepp <daniel.nepp@drei.at>
2023-08-22 22:24:29 +0200

    prepare for release 0.3.1

--- src/main/java/sprouts/impl/package-info.java
+++ src/main/java/sprouts/impl/package-info.java
@@ -1,6 +1,6 @@
 /**
  * <b>WARNING: Do not depend on classes inside this package!</b>
  * Everything in this package should be considered library-private.
- * Code inside the package or any sub-packages might change between minor versions...
+ * Public API code inside the package or any sub-packages might change between minor versions...
  */
 package sprouts.impl;
