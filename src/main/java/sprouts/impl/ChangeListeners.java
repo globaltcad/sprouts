39fbf8ecfce33db9bc2e4430d208e298acafa81a
Daniel Nepp <daniel.nepp@drei.at>
2024-06-22 23:41:26 +0200

    simplify ChangeListeners code

--- src/main/java/sprouts/impl/ChangeListeners.java
+++ src/main/java/sprouts/impl/ChangeListeners.java
@@ -13,7 +13,7 @@ final class ChangeListeners<T>
 {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(ChangeListeners.class);
 
-    private final Map<Channel, List<Action<Val<T>>>> _actions = new LinkedHashMap<>();
+    private final Map<Channel, ChannelListeners<T>> _actions = new LinkedHashMap<>();
 
 
     public ChangeListeners() {}
@@ -23,11 +23,11 @@ final class ChangeListeners<T>
     }
 
     private void _copyFrom(ChangeListeners<T> other) {
-        other._actions.forEach( (k, v) -> _actions.put(k, new ArrayList<>(v)) );
+        other._actions.forEach( (k, v) -> _actions.put(k, new ChannelListeners<>(v)) );
     }
 
-    private List<Action<Val<T>>> _getActionsFor( Channel channel ) {
-        return _actions.computeIfAbsent(channel, k->new ArrayList<>());
+    private ChannelListeners<T> _getActionsFor(Channel channel ) {
+        return _actions.computeIfAbsent(channel, k->new ChannelListeners<>());
     }
 
     private void _addActionTo( Channel channel, Action<Val<T>> action ) {
@@ -35,7 +35,7 @@ final class ChangeListeners<T>
     }
 
     private void _removeActionIf( Predicate<Action<Val<T>>> predicate ) {
-        for ( List<Action<Val<T>>> actions : _actions.values() )
+        for ( ChannelListeners<T> actions : _actions.values() )
             actions.removeIf(predicate);
     }
 
@@ -52,24 +52,12 @@ final class ChangeListeners<T>
 	public void fireChange( Val<T> owner, Channel channel ) {
 		if ( channel == From.ALL)
 			for ( Channel key : _actions.keySet() )
-				_triggerActions( owner, _getActionsFor(key) );
+                _getActionsFor(key).trigger( owner );
 		else {
-			_triggerActions( owner, _getActionsFor(channel) );
-			_triggerActions( owner, _getActionsFor(From.ALL) );
+            _getActionsFor(channel).trigger( owner );
+            _getActionsFor(From.ALL).trigger( owner );
 		}
 	}
-
-    private void _triggerActions(
-        Val<T> owner, List<Action<Val<T>>> actions
-    ) {
-        Val<T> clone = Val.ofNullable(owner); // We clone this property to avoid concurrent modification
-        for ( Action<Val<T>> action : new ArrayList<>(actions) ) // We copy the list to avoid concurrent modification
-            try {
-                action.accept(clone);
-            } catch ( Exception e ) {
-                log.error("An error occurred while executing action '"+action+"' for property '"+this+"'", e);
-            }
-    }
 
     public void unsubscribe( Subscriber subscriber ) {
         _removeActionIf( a -> {
@@ -80,6 +68,37 @@ final class ChangeListeners<T>
                 else
                     return Objects.equals(a, subscriber);
             });
+    }
+
+    private static final class ChannelListeners<T> {
+
+        private final List<Action<Val<T>>> _actions = new ArrayList<>();
+
+
+        public ChannelListeners() {}
+
+        public ChannelListeners(ChannelListeners<T> other ) {
+            _actions.addAll(other._actions);
+        }
+
+        public void add( Action<Val<T>> action ) {
+            _actions.add(action);
+        }
+
+        public void removeIf( Predicate<Action<Val<T>>> predicate ) {
+            _actions.removeIf(predicate);
+        }
+
+        public void trigger( Val<T> owner ) {
+            Val<T> clone = Val.ofNullable(owner); // We clone this property to avoid concurrent modification
+            for ( Action<Val<T>> action : new ArrayList<>(_actions) ) // We copy the list to avoid concurrent modification
+                try {
+                    action.accept(clone);
+                } catch ( Exception e ) {
+                    log.error("An error occurred while executing action '"+action+"' for property '"+this+"'", e);
+                }
+        }
+
     }
 
 }
