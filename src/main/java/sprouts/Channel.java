2417eb912c07fe28ad1ea64090e200793eb9d873
Daniel Nepp <daniel.nepp@drei.at>
2024-08-18 05:00:41 +0200

    add documentation to new delegate type

--- src/main/java/sprouts/Channel.java
+++ src/main/java/sprouts/Channel.java
@@ -4,7 +4,7 @@ package sprouts;
  *  This is a marker interface for property event channels.
  *  Instances of this interface are used to identify the source of a change,
  *  so they are expected to be immutable constants, like enums.
- *  The standard channels are defined in {@link From}.
+ *  The standard channels are defined in the {@link From} enum.
  *  <p>
  *  Please consider using {@link From} instead of implementing this interface directly.
  *  If you do implement this interface directly, please make sure to implement
