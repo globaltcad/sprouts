38b3d70a6cb7e5b23a9f2c8f65e0197b51cb0e35
Daniel Nepp <d.nepp@globaltcad.com>
2025-01-18 17:00:10 +0900

    make event implementation state atomic

--- src/main/java/sprouts/impl/EventImpl.java
+++ src/main/java/sprouts/impl/EventImpl.java
@@ -4,44 +4,62 @@ import org.slf4j.Logger;
 import sprouts.*;
 
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 final class EventImpl implements Observable, Event {
 
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(EventImpl.class);
 
-    private final List<WeakReference<EventImpl>> vessels = new ArrayList<>();
-    private final List<Observer> observers = new ArrayList<>();
     private final Executor executor;
+    private final AtomicReference<Tuple<WeakReference<EventImpl>>> vessels = new AtomicReference<>((Tuple) Tuple.of(WeakReference.class));
+    private final AtomicReference<Tuple<Observer>> observers = new AtomicReference<>(Tuple.of(Observer.class));
+
 
     EventImpl(Executor executor) {
         this.executor = executor;
     }
 
+    @SuppressWarnings("NullAway")
+    private Tuple<WeakReference<EventImpl>> _getVessels() {
+        return vessels.get();
+    }
+
+    private void _setVessels(Tuple<WeakReference<EventImpl>> vessels) {
+        this.vessels.set(vessels);
+    }
+
+    @SuppressWarnings("NullAway")
+    private Tuple<Observer> _getObservers() {
+        return observers.get();
+    }
+
+    private void _setObservers(Tuple<Observer> observers) {
+        this.observers.set(observers);
+    }
+
     @Override
     public void fire() {
         executor.execute(() -> {
-            for (Observer observer : observers) {
+            for (Observer observer : _getObservers()) {
                 try {
                     observer.invoke();
                 } catch (Exception e) {
                     log.error("Error invoking observer!", e);
                 }
             }
-            for (WeakReference<EventImpl> vessel : vessels) {
+            for (WeakReference<EventImpl> vessel : _getVessels()) {
                 EventImpl event = vessel.get();
                 if (event != null)
                     event.fire();
             }
-            vessels.removeIf(vessel -> vessel.get() == null);
+            _setVessels(_getVessels().removeIf(vessel -> vessel.get() == null));
         });
     }
 
     @Override
     public Observable observable() {
         EventImpl vessel = new EventImpl(executor);
-        vessels.add(new WeakReference<>(vessel));
+        _setVessels(_getVessels().add(new WeakReference<>(vessel)));
         return vessel;
     }
 
@@ -50,26 +68,26 @@ final class EventImpl implements Observable, Event {
         if (observer instanceof WeakObserver) {
             WeakObserver<?> weakObserver = (WeakObserver<?>) observer;
             weakObserver.owner().ifPresent(owner -> {
-                observers.add(observer);
+                _setObservers(_getObservers().add(observer));
                 WeakReference<EventImpl> weakThis = new WeakReference<>(this);
                 AutomaticUnSubscriber cleaner = new AutomaticUnSubscriber(weakThis, weakObserver);
                 ChangeListenerCleaner.getInstance().register(owner, cleaner);
             });
         } else
-            observers.add(observer);
+            _setObservers(_getObservers().add(observer));
         return this;
     }
 
     @Override
     public Observable unsubscribe(Subscriber subscriber) {
         if (subscriber instanceof Observer)
-            observers.remove((Observer) subscriber);
+            _setObservers(_getObservers().remove((Observer) subscriber));
         return this;
     }
 
     @Override
     public void unsubscribeAll() {
-        observers.clear();
+        _setObservers(_getObservers().clear());
     }
 
 
@@ -96,7 +114,7 @@ final class EventImpl implements Observable, Event {
                     "removing it from the list of change actions.", observer, e
                 );
             }
-            strongThis.observers.remove(observer);
+            strongThis._setObservers(strongThis._getObservers().remove(observer));
         }
     }
 }
