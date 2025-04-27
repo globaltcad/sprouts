7bf5109422feab64cb852911c00bd141ca94f043
Daniel Nepp <daniel.nepp@drei.at>
2025-04-27 10:19:19 +0900

    fix unit test and improve documentation

--- src/main/java/sprouts/ResultItemSupplier.java
+++ src/main/java/sprouts/ResultItemSupplier.java
@@ -5,7 +5,9 @@ import org.jspecify.annotations.Nullable;
 /**
  *  Supplier of a {@link Result}'s item, which may throw a {@link RuntimeException} or
  *  checked {@link Exception} when used to supply a result item during an invocation
- *  of the {@link Result#ofTry(Class, ResultItemSupplier)} factory method.
+ *  of the {@link Result#ofTry(Class, ResultItemSupplier)} factory method.<br>
+ *  This is a functional interface only intended to be used
+ *  for the result pattern.
  *
  * @param <T> The type of the item to be supplied.
  */
