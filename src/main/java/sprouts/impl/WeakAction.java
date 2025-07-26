d413c7a7691cc205556ace5cd98eb9ef782c9db8
Daniel Nepp <daniel.nepp@drei.at>
2025-07-26 17:39:57 +0900

    remove redundant abstraction (make 'WeakAction' a regular class instead of an interface).

--- src/main/java/sprouts/impl/WeakAction.java
+++ src/main/java/sprouts/impl/WeakAction.java
@@ -1,8 +1,12 @@
 package sprouts.impl;
 
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 import sprouts.Action;
 
+import java.lang.ref.WeakReference;
 import java.util.Optional;
+import java.util.function.BiConsumer;
 
 /**
  *  A weak action is an extension of the {@link Action} interface
@@ -16,8 +20,33 @@ import java.util.Optional;
  * @param <O> The type of the owner of this action.
  * @param <D> The type of the delegate that will be passed to this event handler.
  */
-interface WeakAction<O, D> extends Action<D>
+final class WeakAction<O extends @Nullable Object, D> implements Action<D>
 {
+
+    static <O, D> WeakAction<O, D> of(O owner, BiConsumer<O, D> action ) {
+        return new WeakAction<>(owner, action);
+    }
+
+    private @Nullable BiConsumer<O, D> _action;
+    private final WeakReference<O> _owner;
+
+    private WeakAction(@NonNull O owner, @NonNull BiConsumer<O, D> action ) {
+        _owner  = new WeakReference<>(owner);
+        _action = action;
+    }
+
+    public void accept( D delegate ) {
+        if ( _action == null )
+            return;
+
+        O owner = _owner.get();
+
+        if ( owner != null )
+            _action.accept(owner, delegate);
+        else
+            _action = null;
+    }
+
     /**
      *  Returns an {@link Optional} containing the owner of this action.
      *  If the owner is no longer available, then an empty {@link Optional}
@@ -25,7 +54,9 @@ interface WeakAction<O, D> extends Action<D>
      *
      * @return An {@link Optional} containing the owner of this action.
      */
-    Optional<O> owner();
+    public Optional<O> owner() {
+        return Optional.ofNullable(_owner.get());
+    }
 
     /**
      *  Clears the action, making it no longer executable.
@@ -36,5 +67,7 @@ interface WeakAction<O, D> extends Action<D>
      *      as it may lead to unexpected behavior.
      *  </b>
      */
-    void clear();
+    public void clear() {
+        _action = null;
+    }
 }
