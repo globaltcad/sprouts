9848a74d40a08102377ed104e9ed56ea2c109060
Daniel Nepp <d.nepp@globaltcad.com>
2025-12-25 10:36:35 +0100

    make unit test more robust

--- src/test/groovy/sprouts/Property_Projection_Lenses_Spec.groovy
+++ src/test/groovy/sprouts/Property_Projection_Lenses_Spec.groovy
@@ -614,6 +614,7 @@ class Property_Projection_Lenses_Spec extends Specification {
             )
         and:
             waitForGarbageCollection()
+            waitForGarbageCollection()
 
         expect: 'Source has change listeners for the projection'
             source.numberOfChangeListeners() == 1
